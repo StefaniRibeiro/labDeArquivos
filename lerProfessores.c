ece27b9abde75553cc658ed720528786a6d37069
StefaniRibeiro <stefaniribeiro.08@gmail.com>
2024-10-08 00:33:23 -0300

    Estrutura para professores

---
+++ lerProfessores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_PROFESSORES 100
+#define MAX_NOME 100
+
+
+typedef struct {
+    char nome[MAX_NOME];
+    char departamento[MAX_NOME];
+} Professor;
