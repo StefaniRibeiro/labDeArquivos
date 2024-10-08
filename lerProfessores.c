b3d43e989c1f7b619dd7ccd543c8ca6c733fee43
StefaniRibeiro <stefaniribeiro.08@gmail.com>
2024-10-08 00:46:43 -0300

    inserção de estrutura para alunos

--- lerProfessores.c
+++ lerProfessores.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_PROFESSORES 100
 #define MAX_NOME 100
+#define MAX_DEPARTAMENTO 50
+#define MAX_ALUNOS 100
+#define MAX_PROFESSORES 50
 
 
 typedef struct {
@@ -12,3 +14,11 @@ typedef struct {
 
 Professor professores[MAX_PROFESSORES];
 int contadorProfessores = 0;
+
+typedef struct {
+    char nome[MAX_NOME];
+    char departamento[MAX_DEPARTAMENTO];
+} Aluno;
+
+Aluno alunos[MAX_ALUNOS];
+int contadorAlunos = 0;
